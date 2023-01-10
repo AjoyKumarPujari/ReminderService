b1e7249a330432b01a6057d6feb9d67d911c4dec
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-01-10 20:32:51 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+//const { sendBasicEmail } = require('./services/email-service');
+
+const cron = require('node-cron');
 
 const setupAndStartServer = () =>{
     const app = express();
@@ -13,14 +15,19 @@ const setupAndStartServer = () =>{
     app.listen(PORT, () =>{
         console.log(`SERVER STARTED AT PORT ${PORT}`);
 
-        sendBasicEmail(
-            'super@admin.com',
-            'bookingservice1001@gmail.com',
-            //'ajoykumarpujari@gmail.com',
-            'This is a testing Email',
-            'Hey, How are you, I hope you like the support'
-        )
+        // sendBasicEmail(
+        //     'super@admin.com',
+        //     'bookingservice1001@gmail.com',
+        //     //'ajoykumarpujari@gmail.com',
+        //     'This is a testing Email',
+        //     'Hey, How are you, I hope you like the support'
+        // )
 
+    
+
+        cron.schedule('*/2 * * * *', () => {
+        console.log('running a task two minute');
+        });
     });
 }
 
